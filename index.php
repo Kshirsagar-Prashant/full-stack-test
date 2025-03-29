90032d2d3c18ec77164df39e739495bae8b9ea76
Kshirsagar-Prashant <100287776+Kshirsagar-Prashant@users.noreply.github.com>
2025-03-29 16:08:46 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -16,6 +16,11 @@ $cnt=count($fetchS);
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style> 
  body {
   background-image: linear-gradient(#10324D, #10324D);
@@ -24,12 +29,23 @@ $cnt=count($fetchS);
         display: flex;
         align-items: center;
     }
+    @media screen and (min-width: 768px){
+        #phone{
+            display:none;
+        }
+    }
+    @media screen and (max-width: 767px){
+        #websie{
+            display:none;
+        }
+    }
 </style>
 
 </head>
 <body>
-<input type="hidden" name="tot_cnt" id="tot_cnt" value="<?=$cnt?>">
 <div class="container-fluid mt-3">
+<input type="hidden" name="tot_cnt" id="tot_cnt" value="<?=$cnt?>">
+
 <a href="CRUD.php" class="btn btn-primary">CRUD Operations</a>
     <div class="row pt-3">
         <div class="col-12 text-center text-white ">
@@ -39,7 +55,7 @@ $cnt=count($fetchS);
           <p> Lorem Ipsum dolor sit amet, consectetuer adipiscing elit, Aenean commodo </p>
         </div>
         <div class="col-12">
-            <div class="container">
+            <div class="container" id="websie">
                 <div class="row">
                     <div class="col-lg-4 col-md-4 p-0 m-0">
                         <div class="row vcenter-item" style="height:500px; background-color:lightgray;">
@@ -149,8 +165,85 @@ $cnt=count($fetchS);
                 </div>
             </div>
         </div>
+        <div class="col-12" id="phone">
+            <div class="row" >
+            <div class="col-12 p-0 m-0">
+                        <div class="row " id="accordion">
+                            
+                                <?php
+                                $n=0;
+                                foreach ($fetchS as $menu){
+                                    $fetchimg1 = $database->fetchimg($menu['Section_Name']);
+
+                                ?><div class="col-12">
+                                <a type="button" data-toggle="collapse" href="#collapseOne<?=$n?>" data-bs-slide-to="0" onclick='change_tab(<?php echo "$n,$cnt";?>)' style="width:100%; text-decoration: none !important; text-align:left; color:black; font-size:28px;"  class="btn active bg-white border-0 p-2 m-2 ">
+                                    <img src="images/<?=$fetchimg1[0]['Section_Icon']?>" height="50" width="50" class="float-left"> &nbsp; <?=$menu['Section_Name']?>
+                                    <i class="fa fa-plus-circle float-right pt-2" style="font-size:30px;color:red; "></i>
+                                    <!-- <i class="fa fa-minus-circle float-right pt-2" style="font-size:30px;color:red"></i> -->
+                                </a>
+                                
+                                </div>
+                                <div id="collapseOne<?=$n?>" class="collapse  ml-2 row text-white" data-parent="#accordion">
+                                     <?php
+                        $S_cnt=0;
+                        
+                            $c_Section_Name = $database->fetchAllDataS($menu['Section_Name']); 
+                        $no=0;
+                                foreach ($c_Section_Name as $menu){
+                                    if($no==0){
+                                ?>
+                                <div id="carouselText<?=$S_cnt?>" class="carousel slide p-0 m-0" data-bs-ride="carousel" <?php if($S_cnt != 0){ echo "style='display:none;'";} ?> style="background-repeat: no-repeat, repeat;background-size: 100% 400px; background-image: url('images/<?=$menu['img']?>')">
+                            <div class="carousel-inner text-white text-center vcenter-item m-0 p-0" style="background-color:#63B4C9; opacity:0.8; height:400px; width:100%;">
+                            
+                                <div class="carousel-item active ">
+                                    <span class="p-1" style="background-color: #5598A9;"><?=$menu['Title']?></span>
+                                <h3 ><?=$menu['Sub_Title']?></h3>
+                                <?=$menu['Description']?>
+                                </div>
+                                <?php
+                                }
+                                if($no>=1){
+                                ?>
+                                <div class="carousel-item">
+                                <span class="p-1" style="background-color: #5598A9;"><?=$menu['Title']?></span>
+                                <h3><?=$menu['Sub_Title']?></h3>
+                                <?=$menu['Description']?>
+                                </div>
+                                    <?php
+                                    }
+                                    $no++;
+                                }
+                                    if($members){
+                                    ?>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselText<?=$S_cnt?>" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselText<?=$S_cnt?>" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <?php
+                                    }
+                                    $S_cnt++;
+                                                     
+                                ?>
+                                </div>
+                                <?php
+                                $n++;
+                                }
+                                ?>
+                            
+                            
+                        </div>
+                    </div>
+            </div>
+        </div>
     </div>
 </div>
+
 <script>
     // Synchronize both carousels
 $(document).ready(function() {
